89a456eaa4fbdf6ae52610fea80a1fef240a12cc
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-05-06 00:28:05 -0300

    Adicionando libs bottomSheet para menu do botao. Dificuldade para executar o emulador/web do app para visualizar o resultado.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 import { View } from 'react-native';
@@ -24,14 +25,13 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background,
     }}>
-      
-      <Widget />
-
       <StatusBar 
       style="light"
       backgroundColor='transparent'
       translucent
       />
+      
+      <Widget />
     </View>
   );
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import {TouchableOpacity, Text } from 'react-native';
-import { ChatTeardropDots} from 'phosphor-react-native';
-
+import React, { useRef } from 'react';
+import {TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import { theme } from '../../theme';
 import { styles } from './styles';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 export function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen(){
+    
+  }
+
   return (
     <>
-      <TouchableOpacity>
-        {/* <ChatTeardropDots /> */}
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots 
+        size={24}
+        weight="bold"
+        color={theme.colors.text_on_brand_color}
+        />
       </TouchableOpacity>
+
+      <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={[1, 280]}
+      >
+
+      </BottomSheet>
+
     </>
   );
 }
