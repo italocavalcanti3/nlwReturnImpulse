c5b9ebc2dbfca1a9ddd547f1aa5be641d148fd9c
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-05-05 22:58:36 -0300

    Stage 04 iniciado. Criando projeto Expo React Native e adicionando libs, fontes para iniciar o desenvolvimento

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {TouchableOpacity, Text } from 'react-native';
+import { ChatTeardropDots} from 'phosphor-react-native';
+
+import { styles } from './styles';
+
+export function Widget() {
+  return (
+    <>
+      <TouchableOpacity>
+        {/* <ChatTeardropDots /> */}
+      </TouchableOpacity>
+    </>
+  );
+}
